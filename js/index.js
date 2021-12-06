5dbf309f282f1e373ce9d414fe4f63f756f65b5e
Nicolas Orecchia <nicoaleore@gmail.com>
2021-12-05 22:18:16 -0300

    datos acciones

--- js/index.js
+++ js/index.js
@@ -31,18 +31,61 @@ $("#ON").click(function on () {
     $("#informacion").html("<p>Las obligaciones negociables son titulos de deuda privada. Son muy parecidos a los bonos, con la diferencia de que los emite una empresa. Las empresas pueden necesitar financiamiento por algun proyecto de inversion grande que tengan planificado, y, en lugar de emitir acciones (lo cual incrementaria su capital y disminuiria el porcentaje de propiedad de los socios actuales) pueden elegir colocar deuda. Las obligaciones negociables, al igual que los bonos, cuentan con un flujo de fondo determinado, con un pago de interes cada cierto tiempo y una devolucion de capital estipulada.</p>")
 })
 
+const displayList = (jsonObject) => {
+    jsonObject.forEach(item => {
+        //console.log(item.name)
+        //console.log(item.username)
+        //console.log(item.email)
+
+        const {ticker, nombre, rubro} = item
+        const userDiv = document.createElement("div")
+        const usuarios = `
+        <div id='datos'>
+            <li>
+                <h2>${nombre}</h2>
+                <p>Codigo bursatil: ${ticker}</p>
+                <p><strong>Rubro: ${rubro}</strong></p>
+            </li>
+        </div>
+        `
+    userDiv.innerHTML = usuarios
+    $("#lista").append(userDiv)
+    });
+}
+
+const jqueryData = () => {
+    $.get(`./data/data.json`, (respuesta, estado) => {
+        if(estado=="success") {
+            console.log(respuesta)
+            displayList(respuesta)
+        }
+    })
+}
+
 $("#acciones").click(function accion () {
     $("#informacion").hide();
     $("#informacion").slideDown(1000, ()=> {
         $("#introduccion").fadeOut();
     });
-    $("#informacion").html("<p>Una accion es una parte del capital de una empresa. Las empresas que tienen necesidades de financiamiento pueden necesitar emitir partes de su capital para cubrir proyectos de inversion. Ser tenedor de una accion de cualquier empresa da derecho a cobrar dividendos cuando la empresa presente sus estados financieros trimestrales, e incluso podes asistir a las asambleas de socios. Generalmente, el que invierte en una accion espera una ganancia derivada del aumento del precio del titulo, por lo cual es un activo de renta variable (no tenemos asegurado un porcentaje de rentabilidad).</p>")
+    $("#informacion").html("<p style='font-size:small'>Una accion es una parte del capital de una empresa. Las empresas que tienen necesidades de financiamiento pueden necesitar emitir partes de su capital para cubrir proyectos de inversion. Ser tenedor de una accion de cualquier empresa da derecho a cobrar dividendos cuando la empresa presente sus estados financieros trimestrales, e incluso podes asistir a las asambleas de socios. Generalmente, el que invierte en una accion espera una ganancia derivada del aumento del precio del titulo, por lo cual es un activo de renta variable (no tenemos asegurado un porcentaje de rentabilidad).</p><div class='botones' id='boton'>Algunos ejemplos</button>")
+    let boton = $("#boton")
+    boton.on("click", jqueryData)
 })
+
+//const baseUrl = "https://cloud.iexapis.com/?symbols=MELI,GLOB"
+
+//const baseUrl = "https://rickandmortyapi.com/documentation/#character"
+
 
 $("#cedears").click(function cedear () {
     $("#informacion").hide();
     $("#informacion").slideDown(1000, ()=> {
         $("#introduccion").fadeOut();
     });
-    $("#informacion").html("<p>Un cedear es una accion de una empresa que cotiza en el extranjero. En nuestro mercado es posible acceder a estos titulos mediante certificados de depositos especiales emitidos por una entidad financiera local que los custodia. Este titulo puede ser transferido a una cuenta comitente en el exterior y vendido en dolares, dando origen a un tipo de cambio conocido como 'contado con liqui' o CCL. Al estar constantemente 'copiando' una cotizacion en moneda dura de una accion extranjera, este activo es una buena opcion para el inversor que quiera cubrirse de una posible devaluacion de la moneda.</p>")
+    $("#informacion").html("<p style='font-size: small'>Un cedear es una accion de una empresa que cotiza en el extranjero. En nuestro mercado es posible acceder a estos titulos mediante certificados de depositos especiales emitidos por una entidad financiera local que los custodia. Este titulo puede ser transferido a una cuenta comitente en el exterior y vendido en dolares, dando origen a un tipo de cambio conocido como 'contado con liqui' o CCL. Al estar constantemente 'copiando' una cotizacion en moneda dura de una accion extranjera, este activo es una buena opcion para el inversor que quiera cubrirse de una posible devaluacion de la moneda.</p>")
 })
+
+let boton = $("#boton")
+
+boton.on("click", jqueryData)
+
