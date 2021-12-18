afa40efa8c1810e7137cf8c8f946a4bf1556ab2a
Nicolas Orecchia <nicoaleore@gmail.com>
2021-12-18 12:18:20 -0300

    cambios js y titulos

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ $("#enviarPerfil").click(() => {
 $("#cauciones").click (function caucion () {
     $("#informacion").hide();
     $("#informacion").slideDown(1000, ()=> {
-        $("#introduccion").fadeOut();
+        $("#titulo").fadeOut();
     });
     $("#informacion").html ("<p id='info'>La caucion bursatil se trata de una inversion muy parecida al plazo fijo tradicional, en pesos, por la cual se obtiene un rendimiento. Se pueden operar a un plazo que varia entre 1 y 180 dias.</p>")
 })
@@ -22,7 +22,7 @@ $("#cauciones").click (function caucion () {
 $("#letras").click (function letra () {
     $("#informacion").hide();
     $("#informacion").slideDown(1000, ()=> {
-        $("#introduccion").fadeOut();
+        $("#titulo").fadeOut();
     });
     $("#informacion").html("<p id='info'>Las letras son titulos de deuda de corto plazo del Estado. Su vencimiento es menor al año y generalmente responde a necesidades de financiamiento especificas del ente que lo emita.</p>")
 })
@@ -30,7 +30,7 @@ $("#letras").click (function letra () {
 $("#bonos").click(function bono () {
     $("#informacion").hide();
     $("#informacion").slideDown(1000, ()=> {
-        $("#introduccion").fadeOut();
+        $("#titulo").fadeOut();
     });
     $("#informacion").html("<p id='info'>Los bonos son un titulo de deuda del Estado a mediano y largo plazo. Algunos bonos cotizan en pesos y en dolares, lo que nos permite comprar dolares por medio del mercado de capitales.</p>" + "<div class='botones' id='calculadora'><a href='./calculadora.html'>Calculadora Mep</a></div>")
 })
@@ -38,7 +38,7 @@ $("#bonos").click(function bono () {
 $("#ON").click(function on () {
     $("#informacion").hide();
     $("#informacion").slideDown(1000, ()=> {
-        $("#introduccion").fadeOut();
+        $("#titulo").fadeOut();
     });
     $("#informacion").html("<p id='info'>Las obligaciones negociables son titulos de deuda privada. Son muy parecidos a los bonos, con la diferencia de que los emite una empresa.</p>")
 })
@@ -79,7 +79,7 @@ const jqueryData = () => {
 $("#acciones").click(function accion () {
     $("#informacion").hide();
     $("#informacion").slideDown(1000, ()=> {
-        $("#introduccion").fadeOut();
+        $("#titulo").fadeOut();
     });
     $("#informacion").html("<p id='info'>Una accion es una parte del capital de una empresa. Ser tenedor de una accion de cualquier empresa da derecho a cobrar dividendos cuando la empresa presente sus estados financieros trimestrales, e incluso podes asistir a las asambleas de socios.</p><div class='botones' id='boton'><p>Algunos ejemplos</p></button>")
     let boton = $("#boton")
@@ -93,15 +93,10 @@ $("#acciones").click(function accion () {
     })
 })
 
-//const baseUrl = "https://cloud.iexapis.com/?symbols=MELI,GLOB"
-
-//const baseUrl = "https://rickandmortyapi.com/documentation/#character"
-
-
 $("#cedears").click(function cedear () {
     $("#informacion").hide();
     $("#informacion").slideDown(1000, ()=> {
-        $("#introduccion").fadeOut();
+        $("#titulo").fadeOut();
     });
     $("#informacion").html("<p id='info'>Un cedear es una accion de una empresa que cotiza en el extranjero. Este titulo puede ser transferido a una cuenta comitente en el exterior y vendido en dolares, dando origen a un tipo de cambio conocido como 'contado con liqui' o CCL.</p>")
 })
